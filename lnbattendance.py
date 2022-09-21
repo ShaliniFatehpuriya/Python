2d5ae676d1029ea3589dfd1f3896be39429db6fe
Shalini Fatehpuriya <91320100+ShaliniFatehpuriya@users.noreply.github.com>
2022-09-21 10:19:27 +0530

    Update lnbattendance.py

--- lnbattendance.py
+++ lnbattendance.py
@@ -1,20 +1,23 @@
 def max_streak(wor, days, data):
     streak = []
-    stk=1
-    i=0
-    counter = True
+    count = 0
+    result = 0
     for day in data:
         if day =='P'*wor:
             streak.append(1)
         else:
             streak.append(0)
 
-    for i in range(days-1):
-        if streak[i] == 1 and streak[i+1] ==1:
-            stk+=1
-    return stk
-    
+    for i in range(0,days):
+
+        if (streak[i] == 0):
+            count = 0
+        else:
+            count+= 1
+            result = max(result, count)         
+    return result
+
 e = 5
 w = 7
-each_data = ['PPPPP','PPPPP','PPPPP', 'AAAPP', 'PAPAP', 'AAAAA', 'PAAAP']
+each_data = ['PPPPP','PPPPP', 'AAAPP', 'PAPAP','AAAAA','PPPPP', 'PAAAP']
 print(max_streak(e,w,each_data))
